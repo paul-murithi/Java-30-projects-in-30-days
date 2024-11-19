fd37e07aa8a26f2a5e92f3cdb34d62c37c46a36e
paul-murithi <paulmurithi233@gmail.com>
2024-11-19 09:07:16 +0300

    feat: Add new features and minor updates - Added homepage and associated components. - Created the login page - Added a simple security config - Added simple styling

---
+++ Day-8-Todo-web-app/src/main/java/com/paul/todo/security/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.paul.todo.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+        @Bean
+        SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+                http
+                                .authorizeHttpRequests(auth -> auth
+                                                .requestMatchers("/login", "/css/**", "/js/**").permitAll()
+                                                .anyRequest().authenticated())
+                                .formLogin(form -> form
+                                                .loginPage("/login")
+                                                .defaultSuccessUrl("/home?login-success")
+                                                .permitAll())
+                                .logout(logout -> logout
+                                                .logoutSuccessUrl("/login?logout")
+                                                .permitAll());
+                return http.build();
+        }
+
+        @Bean
+        UserDetailsService userDetailsService() {
+                return new InMemoryUserDetailsManager(
+                                User.withUsername("user")
+                                                .password("{noop}password") // {noop} means no password encoding
+                                                .roles("USER")
+                                                .build());
+        }
+}
