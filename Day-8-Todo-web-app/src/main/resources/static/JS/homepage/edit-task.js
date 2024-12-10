5461902ad2bec275c79296410542334cdd3d080f
paul-murithi <paulmurithi233@gmail.com>
2024-12-10 17:55:55 +0300

    feat:"Added logic to edit a task after creation"

---
+++ Day-8-Todo-web-app/src/main/resources/static/JS/homepage/edit-task.js
@@ -0,0 +1,52 @@
+// Get all edit buttons
+const editButtons = document.querySelectorAll(".task-card-edit");
+
+editButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    // Get the parent task card element
+    const taskCard = button.closest(".task-card");
+
+    // Get the text content of the tasks
+    const taskTitle = taskCard.querySelector(".task-card-title b").textContent;
+    const taskDetail = taskCard.querySelector(".task-card-detail").textContent;
+
+    const taskDate = taskCard.querySelector(".task-date").textContent;
+
+    const taskPriority = taskCard.querySelector(".task-priority").textContent;
+
+    // Open the edit task modal
+    openEditTaskModal(taskTitle, taskDetail, taskDate, taskPriority);
+  });
+});
+
+// Function to open the edit task modal
+function openEditTaskModal(taskTitle, taskDetail, taskDate, taskPriority) {
+  const modal = document.getElementById("editTaskModal");
+
+  // Set the task title, detail, date, priority in the modal as per how the task was
+  const titleInput = modal.querySelector("#title");
+  const detailInput = modal.querySelector("#detail");
+  const dateInput = modal.querySelector("#date");
+  const priorityInput = modal.querySelector("#priority");
+
+  titleInput.value = taskTitle;
+  detailInput.value = taskDetail;
+  dateInput.value = taskDate;
+  priorityInput.value = taskPriority;
+
+  // Show the modal to enable editings
+  modal.style.display = "block";
+}
+
+const closeModal = document.getElementById("close-modal");
+closeModal.addEventListener("click", closeEditTaskModal);
+
+// Handle modal cancel button
+const cancelBtn = document.getElementById("cancel-btn");
+cancelBtn.addEventListener("click", closeEditTaskModal);
+
+// Function to close the edit task modal
+function closeEditTaskModal() {
+  const modal = document.getElementById("editTaskModal");
+  modal.style.display = "none";
+}
