815e0becfe9b74c8961e0366b78869808798d1d9
paul-murithi <paulmurithi233@gmail.com>
2024-12-13 19:57:46 +0300

    feat: Added my account page

--- Day-8-Todo-web-app/src/main/java/com/paul/todo/controller/HomeController.java
+++ Day-8-Todo-web-app/src/main/java/com/paul/todo/controller/HomeController.java
@@ -1,7 +1,13 @@
 package com.paul.todo.controller;
 
+import java.time.LocalDateTime;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.paul.todo.entity.User;
+
+import org.springframework.ui.Model;
 
 @Controller
 public class HomeController {
@@ -24,6 +30,18 @@ public class HomeController {
     @GetMapping("/signup")
     public String signUp() {
         return "auth/signup";
+    }
+
+    @GetMapping("/my-account")
+    public String myAccount(Model model) {
+        User user = new User();
+        user.setUsername("Paul Murithi");
+        user.setEmail("sampleemail@gmail.com");
+        user.setCreatedAt(LocalDateTime.now());
+
+        model.addAttribute("user", user);
+
+        return "auth/my-account";
     }
 
 }
--- Day-8-Todo-web-app/src/main/java/com/paul/todo/entity/User.java
+++ Day-8-Todo-web-app/src/main/java/com/paul/todo/entity/User.java
@@ -35,4 +35,62 @@ public class User {
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Task> tasks;
+
+    // Getters and Setters
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public List<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<Category> categories) {
+        this.categories = categories;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
 }
